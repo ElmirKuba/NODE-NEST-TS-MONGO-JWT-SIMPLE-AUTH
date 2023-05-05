63d481c863ad833e912506b613c8ed5580f2d6c4
Elmir Kuba <elmir_kuba@vk.com>
2023-05-05 19:23:20 +0500

    исправлена ошибка валидации регистрации

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -2,7 +2,7 @@ import { IsString, Length } from 'class-validator';
 
 export class RegisterUserDto {
   @IsString()
-  @Length(5, 30, {
+  @Length(3, 30, {
     message: `Имя пользователя должно быть от 3 до 30 символов (включительно)`,
   })
   username: string;
